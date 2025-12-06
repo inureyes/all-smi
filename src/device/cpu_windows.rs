6c211a88a01d262c849d74f4dc35a8099cfabe8a
Jeongkyu Shin <inureyes@gmail.com>
2025-12-06 17:35:34 +0900

    update: cargo fmt

--- src/device/cpu_windows.rs
+++ src/device/cpu_windows.rs
@@ -12,7 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::device::{CoreType, CoreUtilization, CpuInfo, CpuPlatformType, CpuReader, CpuSocketInfo};
+use crate::device::{
+    CoreType, CoreUtilization, CpuInfo, CpuPlatformType, CpuReader, CpuSocketInfo,
+};
 use crate::utils::system::get_hostname;
 use chrono::Local;
 use serde::Deserialize;
